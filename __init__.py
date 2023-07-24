a1697768ad03e58706e4e1ac813825abdba6e7bb
timonvanhasselt <timonvanhasselt@users.noreply.github.com>
2023-07-24 16:24:25 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,24 +1,24 @@
-from mycroft import MycroftSkill, intent_file_handler
-from mycroft.skills.audioservice import AudioService
-from mycroft.audio import wait_while_speaking
+from ovos_workshop.skills import OVOSSkill
+from ovos_workshop.decorators import intent_handler
+from ovos_audio.utils import wait_while_speaking
+from ovos_plugin_common_play.ocp.mycroft_cps import MycroftAudioService
 
+class NieuwsAfspelen(OVOSSkill):
 
-class NieuwsAfspelen(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super().__init__(name="Nieuws NOS")
+        self.url = "https://cdn.nos.nl/content/radio/ditisdenieuweurlvoorhetradiobulletinvoorgooglevanafjuli2019.mp3"
 
     def initialize(self):
-        self.audio_service = AudioService(self.bus)
+        self.audio_service = MycroftAudioService(self.bus)
 
-    @intent_file_handler('nieuws_afspelen.intent')
-    def handle_afspelen(self, message):
+    @intent_handler('nieuws_afspelen.intent')
+    
+    def play(self, message):
+        self.log.info("Hier is het NOS nieuws")
         self.speak_dialog('nieuws_afspelen')
         wait_while_speaking()
-        self.audio_service.play('https://cdn.nos.nl/content/radio/ditisdenieuweurlvoorhetradiobulletinvoorgooglevanafjuli2019.mp3')
+        self.audio_service.play(self.url)
 
     def stop(self):
         self.audio_service.stop()
-        
-def create_skill():
-    return NieuwsAfspelen()
-
